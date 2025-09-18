7588987d0390f0fa8352593fe952ed826559892e
MathanKumar K <91076873+mathanroot@users.noreply.github.com>
2025-09-18 11:56:45 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,15 @@
-output "ec2_public_ip" {
-  value = aws_instance.strapi_ec2.public_ip
+output "web" {
+  value       = aws_instance.web.public_ip
+  
 }
 
-output "rds_endpoint" {
-  value = aws_db_instance.strapi_db.endpoint
+output "web1" {
+  value       = aws_instance.web.private_ip
+  
 }
 
-output "s3_bucket" {
-  value = aws_s3_bucket.strapi_media.bucket
+output "db" {
+  value       = aws_instance.db.private_ip
+  
 }
+
