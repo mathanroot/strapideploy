62db14b4359d32aa52d8874936981fc879cf2494
MathanKumar K <91076873+mathanroot@users.noreply.github.com>
2025-09-18 11:55:11 +0530

    Delete s3.tf

--- s3.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_s3_bucket" "strapi_media" {
-  bucket = "strapi-media-bucket-${random_id.bucket_suffix.hex}"
-  acl    = "private"
-}
-
-resource "random_id" "bucket_suffix" {
-  byte_length = 4
-}
