b40bb2713f00a4e2b77ec4abed46ed8e2427d107
MathanKumar K <91076873+mathanroot@users.noreply.github.com>
2025-09-18 11:57:46 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,25 +1,7 @@
-resource "aws_vpc" "strapi_vpc" {
-  cidr_block = var.vpc_cidr
-}
+provider "aws" {
+  region = "us-east-1"
 
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.strapi_vpc.id
-}
-
-resource "aws_subnet" "public" {
-  vpc_id            = aws_vpc.strapi_vpc.id
-  cidr_block        = var.public_subnet_cidrs[0]
-  map_public_ip_on_launch = true
-}
-
-
-resource "aws_subnet" "private" {
-  count             = length(var.private_subnet_cidrs)
-  vpc_id            = aws_vpc.strapi_vpc.id
-  cidr_block        = var.private_subnet_cidrs[count.index]
-  availability_zone = data.aws_availability_zones.available.names[count.index]
-  map_public_ip_on_launch = false
-  tags = {
-    Name = "Private-${count.index + 1}"
+  assume_role {
+    role_arn = "arn:aws:iam::yourrole"
   }
 }
